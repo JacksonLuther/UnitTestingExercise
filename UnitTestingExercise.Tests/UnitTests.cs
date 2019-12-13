c997c5aae54b6e43c718de25d758f620a0580d39
Jackson Luther <gr8peshot@gmail.com>
2019-12-12 18:18:13 -0600

    Finished Exercise

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,73 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(5, 6, 7, 18)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            UnitTestMethods challenger = new UnitTestMethods();
             //Act
-
+            int actual = challenger.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10, 9, 1)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            UnitTestMethods challenger = new UnitTestMethods();
             //Act
-
+            int actual = challenger.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(3, 3, 9)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            UnitTestMethods challenger = new UnitTestMethods();
             //Act
-
+            int actual = challenger.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10, 2, 5)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            UnitTestMethods challenger = new UnitTestMethods();
             //Act
-
+            int actual = challenger.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Fact]
-        public void YourMethodName()
+        public void GetHello()
         {
             //Arrange
-
+            UnitTestMethods challenger = new UnitTestMethods();
             //Act
-
+            string actual = challenger.GetHello();
             //Assert
-
+            Assert.Equal("Hello", actual);
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void GetGoHome()
         {
             //Arrange
-
+            UnitTestMethods challenger = new UnitTestMethods();
             //Act
-
+            string actual = challenger.GetGoHome();
             //Assert
+            Assert.Equal("Go Home", actual);
         }
     }
 }
